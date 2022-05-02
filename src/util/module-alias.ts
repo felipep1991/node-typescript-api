a8fdc100e4d8ebc7c12acdddde6d4cc0acd4d0da
felipep1991 <felipepg0@gmail.com>
2022-05-01 21:50:04 -0300

    config prettier

--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -5,5 +5,5 @@ const files = path.resolve(__dirname, '../..');
 
 moduleAlias.addAliases({
   '@src': path.join(files, 'srs'),
-  '@test': path.join(files, 'test')
+  '@test': path.join(files, 'test'),
 });
